79db9d6a240b4c02cac3fe2db7da6fc56bdf62fc
lmarzora <lmarzora@itba.edu.ar>
2017-09-02 18:35:36 -0300

    changed bullet zombie and player mass

--- tp1/Asteroids2D/Assets/Scripts/GameLogic.cs
+++ tp1/Asteroids2D/Assets/Scripts/GameLogic.cs
@@ -7,15 +7,15 @@ public class GameLogic : MonoBehaviour {
 	// BULLET
 	public static float TIME_BETWEEN_SHOTS = 300.0f;
 	public static int BULLET_AMOUNT = 10;
-	public static float BULLET_SPEED = 2000.0f;
+	public static float BULLET_SPEED = 1000.0f;
 	public static float BULLET_TTL = TIME_BETWEEN_SHOTS * (BULLET_AMOUNT - 1);
 	// CAMERA
 	public static float CAMERA_DISTANCE = 10.0f;
 	// CHARACTER
-	public static int CHARACTER_ROTATION_SPEED = 400;
-	public static float CHARACTER_VELOCITY = 40.0f;
+	public static int CHARACTER_ROTATION_SPEED = 200;
+	public static float CHARACTER_VELOCITY = 800.0f;
 	// ZOMBIES
-	public static float ZOMBIE_VELOCITY = 10f;
+	public static float ZOMBIE_VELOCITY = 100f;
 	public static float ZOMBIE_SHOW_BODY = 3000.0f;
 	public static int ZOMBIE_AMOUNT = 20;
 	public static double ZOMBIE_TIME_BETWEEN_SPAWNS = 1000.0f;
