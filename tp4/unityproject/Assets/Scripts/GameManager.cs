c47b20b04d9795037ce5afe1817408875c3016ee
alebian <alebezdjian@gmail.com>
2017-11-17 13:10:24 -0300

    Added some prefabs

---
+++ tp4/unityproject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourSingleton<GameManager> {
+	private Level level;
+
+	public GameObject floorPrefab;
+	public GameObject wallPrefab;
+
+	void Start() {
+		this.level = LevelManager.Instance.CreateLevel (5, 5);
+		DrawTiles ();
+	}
+
+	private void DrawTiles() {
+		for (int row = 0; row < level.map.GetLength(0); row++) {
+			for (int col = 0; col < level.map.GetLength(1); col++) {
+				GameObject go;
+				Vector3 position = new Vector3 (row, col, 0);
+				switch (level.map [row, col]) {
+				case Level.Tile.Floor:
+					go = Instantiate (floorPrefab, position, Quaternion.identity) as GameObject;	
+					break;
+				case Level.Tile.Wall:
+					go = Instantiate (wallPrefab, position, Quaternion.identity) as GameObject;
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ tp4/unityproject/Assets/Scripts/Models/Level.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Level
+{
+	public enum Tile { Empty, Wall, Floor };
+
+	public Tile[,] map;
+
+	public Level (Tile[,] map)
+	{
+		this.map = map;
+	}
+}
